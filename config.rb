bd0515434c5967a91d987484a6db0d1921f1b9be
Jesus Leon <git@jes.mx>
2016-03-14 23:56:34 +0100

    Add default issue comment

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
 HOST = 'https://your.redmine.installation/'
 USER = 'your_username'
 PASSWORD = 'your_password'
-DEFAULT_ISSUE_ID = 5653
+DEFAULT_ISSUE_ID = 5653 # if no mirrored case is foung, it will use this issue_id as fallback.
